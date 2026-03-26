71d05e2775162c2a597e313f7133b594dde1a88c
marukun712 <marukun530@gmail.com>
2026-03-26 12:24:51 +0900

    First Commit

---
+++ spec/n-high-lovelive_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe N::High::Lovelive do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/n-high-lovelive.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `N::High::Lovelive`
+module N::High::Lovelive
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
