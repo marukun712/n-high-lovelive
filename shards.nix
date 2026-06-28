1b61854aa2a41b26ddbf824a66539b37b7d91367
marukun712 <marukun530@gmail.com>
2026-06-28 11:39:18 +0900

    nix

---
+++ shards.nix
@@ -0,0 +1,32 @@
+{
+  "backtracer" = {
+    url = "https://github.com/sija/backtracer.cr.git";
+    rev = "v1.2.4";
+    sha256 = "0v4wcf5lchgv9amsf5w9ajxmnp4gn0qmsxwzajbd8hs3a0dqzxkg";
+  };
+  "exception_page" = {
+    url = "https://github.com/crystal-loot/exception_page.git";
+    rev = "v0.5.0";
+    sha256 = "04x9yx3bkwm53zdra87dg2c51l9qdkn1axh3n5p9v85vbhi9chij";
+  };
+  "kemal" = {
+    url = "https://github.com/kemalcr/kemal.git";
+    rev = "v1.10.1";
+    sha256 = "0xg28r09c7phhzm3ihnpj2534i1abc9z3qjlzdgvg9rp1vfazc8x";
+  };
+  "markd" = {
+    url = "https://github.com/icyleaf/markd.git";
+    rev = "v0.5.0";
+    sha256 = "1a677z57kwjq6lp4ws7br1ga8jgpgi8990glhd1r8756bdyd8mg0";
+  };
+  "radix" = {
+    url = "https://github.com/luislavena/radix.git";
+    rev = "v0.4.1";
+    sha256 = "1l08cydkdidq9yyil1wl240hvk41iycv04jrg6nx5mkvzw4z1bzg";
+  };
+  "sanitize" = {
+    url = "https://github.com/straight-shoota/sanitize.git";
+    rev = "c17d933fed22c6d3d615cca91a895166414389af";
+    sha256 = "0xavqn955chcygr40ivp830i51q21kqqg0s13fpwynyg05kjxh7x";
+  };
+}
